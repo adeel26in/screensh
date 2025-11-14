b3a12e569a0949271905202073e630b6c356ce47
adeel26in <mohammadhammad18032012@gmail.com>
2025-11-14 16:50:54 +0100

    Create screensh_windows.go

---
+++ src/screensh_windows.go
@@ -0,0 +1,46 @@
+func windows() {
+	fmt.Println("Welcome to screensh!")
+
+	fmt.Println("SCREENSHOTS AFTER 2 SECONDS!")
+
+	var fileaddress string
+	var filename string
+	fmt.Println("EXAMPLE: /home/<user>/examplefolder/")
+
+	fmt.Print("Where do you want to save the file?: ")
+	fmt.Scanln(&fileaddress)
+
+	fmt.Println("EXAMPLE (ONLY PNG): example.png")
+
+	fmt.Print("What do you want to name the file?: ")
+	fmt.Scanln(&filename)
+
+	fullpath := filepath.Join(fileaddress, filename)
+
+	time.Sleep(2 * time.Second)
+	
+	actualimage, err := screenshot.CaptureScreen()
+
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+
+	filecreate, err := os.Create(fullpath)
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+
+	defer filecreate.Close()
+
+	err = png.Encode(filecreate, actualimage)
+
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+}
