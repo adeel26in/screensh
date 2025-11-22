93c615ba5fe64737cae11297fecda808e4474e9c
adeel26in <mohammadhammad18032012@gmail.com>
2025-11-22 13:42:05 +0100

    Update screensh_funcs.go

--- src/screensh_funcs.go
+++ src/screensh_funcs.go
@@ -12,7 +12,6 @@ import (
 )
 
 func linux() {
-
 	fmt.Println("Welcome to screensh!")
 
 	fmt.Println("REQUIRES GNOME-SCREENSHOT!")
@@ -36,7 +35,6 @@ func linux() {
 	cmd := exec.Command("gnome-screenshot", "--include-pointer", "--delay", "2", "-f", fullpath)
 
 	err := cmd.Run()
-
 	if err != nil {
 
 		fmt.Println(err)
@@ -44,9 +42,7 @@ func linux() {
 	}
 }
 
-
 func darwin() {
-
 	fmt.Println("Welcome to screensh!")
 
 	fmt.Println("SCREENSHOTS AFTER 2 SECONDS!")
@@ -67,7 +63,6 @@ func darwin() {
 
 	cmd := exec.Command("screencapture", "-a", "-C", "-x", "-T", "2", fullpath)
 	err := cmd.Run()
-
 	if err != nil {
 
 		fmt.Println(err)
@@ -95,9 +90,8 @@ func windows() {
 	fullpath := filepath.Join(fileaddress, filename)
 
 	time.Sleep(2 * time.Second)
-	
-	actualimage, err := screenshot.CaptureScreen()
 
+	actualimage, err := screenshot.CaptureScreen()
 	if err != nil {
 
 		fmt.Println(err)
@@ -114,7 +108,51 @@ func windows() {
 	defer filecreate.Close()
 
 	err = png.Encode(filecreate, actualimage)
+	if err != nil {
 
+		fmt.Println(err)
+		return
+	}
+}
+
+func freebsd() {
+	fmt.Println("Welcome to screensh!")
+
+	fmt.Println("SCREENSHOTS AFTER 2 SECONDS!")
+
+	var fileaddress string
+	var filename string
+	fmt.Println("EXAMPLE: /home/<user>/examplefolder/")
+
+	fmt.Print("Where do you want to save the file?: ")
+	fmt.Scanln(&fileaddress)
+
+	fmt.Println("EXAMPLE (ONLY PNG): example.png")
+
+	fmt.Print("What do you want to name the file?: ")
+	fmt.Scanln(&filename)
+
+	fullpath := filepath.Join(fileaddress, filename)
+
+	time.Sleep(2 * time.Second)
+
+	actualimage, err := screenshot.CaptureScreen()
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+
+	filecreate, err := os.Create(fullpath)
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+
+	defer filecreate.Close()
+
+	err = png.Encode(filecreate, actualimage)
 	if err != nil {
 
 		fmt.Println(err)
